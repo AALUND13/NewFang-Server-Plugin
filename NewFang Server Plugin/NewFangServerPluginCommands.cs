542c630b97679b6a744f4856ddf26e60541632d1
AALUND13 <tamlund1976@gmail.com>
2024-08-01 17:01:10 -0400

    Added "!Planets list" command.

--- NewFang Server Plugin/NewFangServerPluginCommands.cs
+++ NewFang Server Plugin/NewFangServerPluginCommands.cs
@@ -1,12 +1,64 @@
 ﻿using NewFangServerPlugin;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Entities.Planet;
 using Sandbox.Game.Multiplayer;
+using Sandbox.Game.Screens.Helpers;
+using Sandbox.Game.World;
+using Sandbox.ModAPI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using Torch.Commands;
 using Torch.Commands.Permissions;
 using VRage.Game.ModAPI;
 
 namespace NewFangServerPlugin {
+    [Category("Planets")]
+    public class PlanetsCommandCategory : CommandModule {
+
+        private static readonly FieldInfo _playerGpssField = typeof(MyGpsCollection).GetField("m_playerGpss", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        [Command("list", "Get the GPS Coordinates of all planets.")]
+        [Permission(MyPromoteLevel.None)]
+        public void GPSAllPlanets() {
+            if(Context.Player == null) {
+                Context.Respond("You must be in-game to use this command.");
+                return;
+            }
+
+            var playerGpss = _playerGpssField.GetValue(MySession.Static.Gpss) as Dictionary<long, Dictionary<int, MyGps>>;
+            List<MyGps> planetGpss = new List<MyGps>();
+
+            List<MyPlanet> planets = MyPlanets.GetPlanets();
+
+            if(planets.Count == 0) {
+                Context.Respond("No planets found.");
+                return;
+            }
+
+            foreach(MyPlanet planet in planets) {
+                planetGpss.Add(new MyGps() {
+                    Name = $"Planet: {planet.Generator.Id.SubtypeName}",
+                    Coords = planet.PositionComp.GetPosition(),
+                    ShowOnHud = true,
+                    AlwaysVisible = true,
+                    GPSColor = new VRageMath.Color(226, 234, 244),
+                    DiscardAt = null,
+                    Description = $"Planet: {planet.Generator.Id.SubtypeName} - {planet.PositionComp.GetPosition()}"
+                });
+            }
+
+            foreach (MyGps planetGps in planetGpss)
+            {
+                if(!playerGpss.ContainsKey(Context.Player.IdentityId) || !playerGpss[Context.Player.IdentityId].Values.ToList().Any(gps => gps.Coords == planetGps.Coords))
+                    MyAPIGateway.Session.GPS.AddGps(Context.Player.IdentityId, planetGps);
+            }
+        }
+    }       
+
+
     [Category("RemoteAPI")]
-    public class NewFang_Server_PluginCommands : CommandModule {
+    public class RemoteAPICommandCatgory : CommandModule {
 
         public NewFangServerPlugin Plugin => NewFangServerPlugin.Instance;
         
